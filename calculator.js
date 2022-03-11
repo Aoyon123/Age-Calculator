58e44ad492cff6a6b0db7a9165be6473040b7cd7
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2022-03-11 12:24:51 +0600

    final

--- calculator.js
+++ calculator.js
@@ -10,9 +10,9 @@ var today = new Date();
 
 let d = today.getDate();
 let m = today.getMonth() + 1;
-console.log(m);
 let y = today.getFullYear();
 
+let maxDays = 0;
 let monthNum = 1;
 
 btn.addEventListener("click", (days, month, year) => {
@@ -31,37 +31,37 @@ btn.addEventListener("click", (days, month, year) => {
     monthNum = 1;
   } else if (m == 2) {
     maxDays = 28;
-    monthNum = 1;
+    monthNum = 2;
   } else if (m == 3) {
     maxDays = 31;
-    monthNum = 1;
+    monthNum = 3;
   } else if (m == 4) {
     maxDays = 30;
-    monthNum = 1;
+    monthNum = 4;
   } else if (m == 5) {
     maxDays = 31;
-    monthNum = 1;
+    monthNum = 5;
   } else if (m == 6) {
     maxDays = 30;
-    monthNum = 1;
+    monthNum = 6;
   } else if (m == 7) {
     maxDays = 31;
-    monthNum = 1;
+    monthNum = 7;
   } else if (m == 8) {
     maxDays = 31;
-    monthNum = 1;
+    monthNum = 8;
   } else if (m == 9) {
     maxDays = 30;
-    monthNum = 1;
+    monthNum = 9;
   } else if (m == 10) {
     maxDays = 31;
-    monthNum = 1;
+    monthNum = 10;
   } else if (m == 11) {
     maxDays = 30;
-    monthNum = 1;
-  } else if (m == 12) {
+    monthNum = 11;
+  } else {
     maxDays = 31;
-    monthNum = 1;
+    monthNum = 12;
   }
 
   var ageYears = y - year;
